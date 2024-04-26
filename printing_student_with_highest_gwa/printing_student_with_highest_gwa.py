7821d7490b31debdde06ac727545db93d1660230
Celine Paguio <celinerosespaguio@iskolarngbayan.pup.edu.ph>
2024-04-26 10:39:34 +0800

    Pseudocode

---
+++ printing_student_with_highest_gwa/printing_student_with_highest_gwa.py
@@ -0,0 +1,7 @@
+# This program will read a binary file containing students name and their GWA and print the name of the student with the highest GWA including his/her GWA.
+
+# Import the pickle module to read binary file 
+# Read the binary file
+# Load the students dictionary
+# Determine the student with the highest GWA
+# Print the student with the highest GWA
