d460186e8b7280f7a155e46002683b31d0ffe07e
Celine Paguio <celinerosespaguio@iskolarngbayan.pup.edu.ph>
2024-04-26 11:33:10 +0800

    Determine the cube of odd integers

--- getting_the_square_and_cube_of_sorted_integers/getting_the_square_and_cube_of_sorted_integers.py
+++ getting_the_square_and_cube_of_sorted_integers/getting_the_square_and_cube_of_sorted_integers.py
@@ -14,14 +14,16 @@ with open("even.txt", "a") as even_file:
             even_integers = num
 # Determine the square of even integers
             square_of_even = (even_integers ** 2)
-            print(square_of_even)
+            # print(square_of_even)
             even_file.write(str(even_integers) + "\n")
         else:
             odd_integers = num
+# Determine the cube of odd integers
+            cube_of_odd = (odd_integers ** 3)
+            print(cube_of_odd)
 # Write odd integers to odd.txt
             with open("odd.txt", "a") as odd_file:
                 odd_file.write(str(odd_integers)+"\n")
-# Determine the square of even integers
-# Determine the cube of odd integers
+
 # Write the square of even integers to double.txt
 # Write the cube of odd integers to triple.txt
