e4808295840ac2ad9155d271b781102c61200b54
Celine Paguio <celinerosespaguio@iskolarngbayan.pup.edu.ph>
2024-04-26 10:22:37 +0800

    Read numbers.txt file

--- sorting_even_and_odd_numbers/sorting_even_and_odd_numbers.py
+++ sorting_even_and_odd_numbers/sorting_even_and_odd_numbers.py
@@ -1,6 +1,10 @@
 # This program will read a text file named "numbers.txt" and will categorize the numbers as odd or even
 
+# Start of program
 # Read numbers.txt file
+with open("numbers.txt", 'r') as numbers_file:
+    numbers = numbers_file.read().splitlines()
+    print(numbers)
 # Convert string to integers
 # Categorize the numbers as even or odd
 # Write even numbers to even.txt
