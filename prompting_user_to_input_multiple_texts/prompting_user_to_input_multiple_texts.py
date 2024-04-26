841857f671e2d42a7f6e4dba8816152128a25eed
Celine Paguio <celinerosespaguio@iskolarngbayan.pup.edu.ph>
2024-04-26 11:00:36 +0800

    Write my_file.txt and ask for user input

--- prompting_user_to_input_multiple_texts/prompting_user_to_input_multiple_texts.py
+++ prompting_user_to_input_multiple_texts/prompting_user_to_input_multiple_texts.py
@@ -1,6 +1,10 @@
 # This program will prompt the user to input texts then write the text contents into my_file.txt
 
+# Start of program
 # Create a file named my_file.txt
+with open("my_file.txt", "w") as my_file:
 # Ask the user for input
+    line = input("Enter line: ")
+    my_file.write(line + "\n")
 # Ask the user if they want to add more lines
 # Print a message once the user don't want to add more lines or if they entered an incorrect key
