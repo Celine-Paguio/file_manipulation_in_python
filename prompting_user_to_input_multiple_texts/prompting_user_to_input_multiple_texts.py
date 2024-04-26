61e378d32a6d44cb5dcbc74a163f40aa5b397e23
Celine Paguio <celinerosespaguio@iskolarngbayan.pup.edu.ph>
2024-04-26 11:05:08 +0800

    Ask the user if they want to add more lines

--- prompting_user_to_input_multiple_texts/prompting_user_to_input_multiple_texts.py
+++ prompting_user_to_input_multiple_texts/prompting_user_to_input_multiple_texts.py
@@ -4,7 +4,13 @@
 # Create a file named my_file.txt
 with open("my_file.txt", "w") as my_file:
 # Ask the user for input
-    line = input("Enter line: ")
-    my_file.write(line + "\n")
+    while True:
+        line = input("Enter line: ")
+        my_file.write(line + "\n")
 # Ask the user if they want to add more lines
+        more_lines = input("Are there more lines y/n? ") 
+        if more_lines.lower() == "y":
+            continue
+        else:
+            break
 # Print a message once the user don't want to add more lines or if they entered an incorrect key
