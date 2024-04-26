63febc4c46e1ef449aa076efd1dc0fa34f45b9ca
Celine Paguio <celinerosespaguio@iskolarngbayan.pup.edu.ph>
2024-04-26 10:49:53 +0800

    Print the student with the highest GWA

--- printing_student_with_highest_gwa/printing_student_with_highest_gwa.py
+++ printing_student_with_highest_gwa/printing_student_with_highest_gwa.py
@@ -1,5 +1,6 @@
 # This program will read a binary file containing students name and their GWA and print the name of the student with the highest GWA including his/her GWA.
 
+# Start of program
 # Import the pickle module to read binary file 
 import pickle
 # Read the binary file
@@ -10,5 +11,7 @@ with open("students.bin", "rb") as file:
 highest_student = min(students, key=students.get)
 # Determine the GWA of the highest student
 highest_gwa = students[highest_student]
-print(highest_gwa)
 # Print the student with the highest GWA
+print("The student who obtained the highest GWA is", highest_student, "with a GWA of %.2f" % highest_gwa)
+
+# End of program
