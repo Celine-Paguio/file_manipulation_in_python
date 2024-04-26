ead02347f9b06e20b04f39c53cd1c88b0c7c8091
Celine Paguio <celinerosespaguio@iskolarngbayan.pup.edu.ph>
2024-04-26 10:57:39 +0800

    Pseudocode

---
+++ prompting_user_to_input_multiple_texts/prompting_user_to_input_multiple_texts.py
@@ -0,0 +1,6 @@
+# This program will prompt the user to input texts then write the text contents into my_file.txt
+
+# Create a file named my_file.txt
+# Ask the user for input
+# Ask the user if they want to add more lines
+# Print a message once the user don't want to add more lines or if they entered an incorrect key
