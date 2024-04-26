9227635021e4318d0b67f40cf79f254e9ef0436e
Celine Paguio <celinerosespaguio@iskolarngbayan.pup.edu.ph>
2024-04-26 10:20:36 +0800

    Pseudocode

---
+++ sorting_even_and_odd_numbers/sorting_even_and_odd_numbers.py
@@ -0,0 +1,7 @@
+# This program will read a text file named "numbers.txt" and will categorize the numbers as odd or even
+
+# Read numbers.txt file
+# Convert string to integers
+# Categorize the numbers as even or odd
+# Write even numbers to even.txt
+# Write odd numbers to odd.txt
