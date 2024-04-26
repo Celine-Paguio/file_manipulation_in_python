f994cae63ef894e5a2ab15421aeee1142cf677ee
Celine Paguio <celinerosespaguio@iskolarngbayan.pup.edu.ph>
2024-04-26 11:37:43 +0800

    Write the square of even integers to double.txt

--- getting_the_square_and_cube_of_sorted_integers/getting_the_square_and_cube_of_sorted_integers.py
+++ getting_the_square_and_cube_of_sorted_integers/getting_the_square_and_cube_of_sorted_integers.py
@@ -7,23 +7,23 @@ with open("integers.txt", "r") as integers_file:
 # Convert string to integer
     integers = [int(num) for num in integers]
 # Categorize the integers to even or odd
-# Write even integers to even.txt
-with open("even.txt", "a") as even_file:
+# Write the square of even integers to double.txt
+with open("double.txt", "a") as double_file:
     for num in integers:
         if num %2 == 0:
             even_integers = num
 # Determine the square of even integers
             square_of_even = (even_integers ** 2)
-            # print(square_of_even)
-            even_file.write(str(even_integers) + "\n")
+            double_file.write(str(square_of_even) + "\n")
+# Write even integers to even.txt
+            with open("even.txt", "a") as even_file:
+                even_file.write(str(even_integers) + "\n")
         else:
             odd_integers = num
 # Determine the cube of odd integers
             cube_of_odd = (odd_integers ** 3)
-            print(cube_of_odd)
 # Write odd integers to odd.txt
             with open("odd.txt", "a") as odd_file:
                 odd_file.write(str(odd_integers)+"\n")
 
-# Write the square of even integers to double.txt
 # Write the cube of odd integers to triple.txt
