9faa2b89ab6ec1dd79aff47dfcc790c20298eb44
Celine Paguio <celinerosespaguio@iskolarngbayan.pup.edu.ph>
2024-04-26 10:31:06 +0800

    Write odd numbers to odd.txt

--- sorting_even_and_odd_numbers/sorting_even_and_odd_numbers.py
+++ sorting_even_and_odd_numbers/sorting_even_and_odd_numbers.py
@@ -16,5 +16,6 @@ with open("even.txt", "a") as even_file:
             even_file.write(str(even_numbers) + "\n")
         else:
             odd_numbers = num
-
 # Write odd numbers to odd.txt
+            with open("odd.txt", "a") as odd_file:
+                odd_file.write(str(odd_numbers)+"\n")
