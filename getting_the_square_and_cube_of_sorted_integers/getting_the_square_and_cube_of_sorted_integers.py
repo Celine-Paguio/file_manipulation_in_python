36c084724245af555e8872f28074190b93459e3a
Celine Paguio <celinerosespaguio@iskolarngbayan.pup.edu.ph>
2024-04-26 11:15:47 +0800

    Pseudocode

---
+++ getting_the_square_and_cube_of_sorted_integers/getting_the_square_and_cube_of_sorted_integers.py
@@ -0,0 +1,11 @@
+# This program will read a text file named "numbers.txt" and will categorize the numbers as odd or even. After categorizing, it will get the square of even integers then write it in double.txt then get the cube of odd integers and write it in triple.txt. 
+
+# Read integers.txt file
+# Convert string to integer
+# Categorize the integers to even or odd
+# Write even numbers to even.txt
+# Write odd numbers to odd.txt
+# Determine the square of even numbers
+# Determine the cube of odd numbers
+# Write the square of even numbers to double.txt
+# Write the cube of odd numbers to triple.txt
